8db746bc01fa5803b203eb7ddbe50a30cb997f20
Adam <adam.johnston00@gmail.com>
2023-12-26 14:03:12 -0800

    swap one random d25 to another

--- day25/main.py
+++ day25/main.py
@@ -1,37 +1,23 @@
 from random import choice
+from collections import defaultdict
 
 def parse(lines):
-    edges = []
-    nodes = set()
+    graph = defaultdict(set)
     for line in lines:
         this, r = line.split(': ')
-        nodes.add(this)
         for other in r.split():
-            nodes.add(other)
-            edges.append((this, other))
-    return nodes, edges
+            graph[this].add(other)
+            graph[other].add(this)
+    return graph
 
-def karger(nodes, edges):
-    ntn = {node: {node} for node in nodes}
-    nodesets = list(ntn.values())
-    while len(nodesets) > 2:
-        a, b = choice(edges)
-        if ntn[a] == ntn[b]:
-            continue
-        ntn[a].update(ntn[b])
-        for c in ntn[b]:
-            if ntn[c] in nodesets:
-                nodesets.remove(ntn[c])
-            ntn[c] = ntn[a]
-    cut = 0
-    for a, b in edges:
-        if ntn[a] is not ntn[b]:
-            cut += 1
-    return cut, nodesets
+def solve(lines):
+    graph = parse(lines)
+    partition = set(graph)
 
-def solve_p1(lines):
-    nodes, edges = parse(lines)
-    while True:
-        cut, groups = karger(nodes, edges)
-        if cut == 3:
-            return len(groups[0]) * len(groups[1])
+    def count_out_edges(node):
+        return len(graph[node] - partition)
+
+    while sum(map(count_out_edges, partition)) != 3:
+        partition.remove(max(partition, key=count_out_edges))
+
+    return len(partition) * len(set(graph) - partition)
